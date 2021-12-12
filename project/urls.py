f0560e71eed876c24e67c348e65a680002451736
Deepak Rajpurohit <princerajpurohit945@gmail.com>
2021-12-12 12:58:37 +0530

    v2

--- project/urls.py
+++ project/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -34,7 +36,6 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sales_app.urls')),
@@ -46,8 +47,9 @@ urlpatterns = [
          name='schema-swagger-ui-download'),
     path('redoc/', schema_view.with_ui('redoc',
                                        cache_timeout=0), name='schema-redoc'),
+    re_path(r'^static/(?P<path>.*)$',serve,{'document_root': 'static'})
 
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
 if settings.DEBUG == True:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
